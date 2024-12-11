c08fa8fe66294419736a42b7576a4691af95c921
mleoncummins <martinleon1@gmail.com>
2024-12-10 19:10:07 -0800

    Add Tesla stock tracking dashboard

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TeslaTracker from './components/TeslaTracker';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-900 text-white">
+      <TeslaTracker />
+    </div>
+  );
+}
+
+export default App;
